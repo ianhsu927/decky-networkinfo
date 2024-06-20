1e4ec946826ba43e0f598820a12d0ef539e24bb0
Ian Hsu <ian927@qq.com>
2024-06-20 21:34:39 +0800

    升级loader

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import subprocess
 
+
 class Plugin:
     async def get_ip_address(self):
-        info = subprocess.run(["ip", "-j", "address"], timeout=10, text=True, capture_output=True).stdout
+        info = subprocess.run(
+            ["ip", "-j", "address"], timeout=10, text=True, capture_output=True
+        ).stdout
         return info
-
